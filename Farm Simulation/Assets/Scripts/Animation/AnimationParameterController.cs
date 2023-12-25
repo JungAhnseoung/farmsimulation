a2edc5b794336a1a6c795f103726a89d28d7674c
JungAhnseoung <80453220+JungAhnseoung@users.noreply.github.com>
2023-12-26 02:51:16 +0900

    Project, Player, Animation set up

---
+++ Farm Simulation/Assets/Scripts/Animation/AnimationParameterController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationParameterController : MonoBehaviour
+{
+    private void AnimationEvent()
+    {
+       
+    }
+}
